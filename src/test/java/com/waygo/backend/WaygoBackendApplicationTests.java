8acb1ab0971d641b92f40a669122861418418b3b
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-05-17 18:41:07 +0500

    chore: add test job to pipeline and a sample test

---
+++ src/test/java/com/waygo/backend/WaygoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waygo.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class WaygoBackendApplicationTests {
+
+	@Test
+	void simpleTest() {
+		assertTrue(true, "This test should always pass");
+	}
+
+}
